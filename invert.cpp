45a937ecb599daeb85c530d820f2177acdf0ea81
aryk2 <aryk2@pdx.edu>
2019-04-26 13:52:33 -0700

    added notes for what is left to finish

--- invert.cpp
+++ invert.cpp
@@ -5,3 +5,8 @@ void invert(float ** & matrix, int size) {
     float scalar = 1 / det;
     scalar_mult(matrix, scalar, size, size);
 }
+
+//wat is left to fix
+//cofactors, (every other negative)
+//adjugate, (flip)
+//then multiply by 1/determinant
