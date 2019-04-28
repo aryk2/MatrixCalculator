1c2f01c6c87a2e757e8a123392b56f48863a8c29
aryk2 <aryk2@pdx.edu>
2019-04-27 20:39:27 -0700

    working on inverse

--- invert.cpp
+++ invert.cpp
@@ -3,10 +3,21 @@
 void invert(float ** & matrix, int size) {
     float det = determinant(matrix, size);
     float scalar = 1 / det;
+    int negate = 0;
+    for(int i = 0; i < size; ++i) {
+        for(int k = 0; k < size; ++k) {
+            if(negate % 2 != 0)
+                matrix[i][k] = matrix[i][k] *-1;
+            ++negate;
+        }
+    }
+    
     scalar_mult(matrix, scalar, size, size);
 }
 
 //wat is left to fix
-//cofactors, (every other negative)
+
+//do I need to find cofactor matrix??
+
 //adjugate, (flip)
 //then multiply by 1/determinant
